479b2dca4bd4f741d04107a92e587d552b5a3368
Olle Kvarnström <olle+git@iix.se>
2016-06-08 21:56:02 +0200

    Test angular

--- html/app.js
+++ html/app.js
@@ -4,12 +4,19 @@
     var app = angular.module('main', []);
 
     app.controller('NotesController', function() {
-       this.posts = [
-           {
-               href: "/2016/03/24/awk.html",
-               date: "24 Mar 2016",
-               title: "Awk",
-           },
-       ];
+       this.posts = note_posts;
     });
+
+    var note_posts = [
+        {
+            href: "/2016/03/24/awk.html",
+            date: "24 Mar 2016",
+            title: "Awk",
+        },
+        {
+            href: "/2016/01/14/installing_spamassassin.html",
+            date: "14 Jan 2016",
+            title: "Installing Spamassassin",
+        },
+    ];
 })();
