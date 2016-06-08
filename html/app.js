abf816ee2f34eca1452420dc5395e193733d267a
Olle Kvarnström <olle+git@iix.se>
2016-06-08 21:47:29 +0200

    Test angular

--- html/app.js
+++ html/app.js
@@ -1,3 +1,15 @@
-'use strict';
+(function() {
+    'use strict';
 
-var app = angular.module('main', []);
+    var app = angular.module('main', []);
+
+    app.controller('NotesController', function() {
+       this.notes = [
+           {
+               href: "/2016/03/24/awk.html",
+               date: "24 Mar 2016",
+               title: "Awk",
+           },
+       ];
+    });
+})();
