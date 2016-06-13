09d0c0b3743021071baca30c4b8e5304e5a75efb
Olle Kvarnström <olle+git@iix.se>
2016-06-13 10:18:29 +0200

    Reverse index

--- html/js/app.js
+++ html/js/app.js
@@ -7,33 +7,39 @@
 
     app.config(['$routeProvider', function($routeProvider) {
         $routeProvider
-            .when("/", {templateUrl: "/partials/main.html"})
+            .when('/', {templateUrl: '/partials/main.html'})
             
             // Notes
-            .when("/notes", {
-                templateUrl: "/partials/notes.html",
-                controller: "NotesController"
+            .when('/notes', {
+                templateUrl: '/partials/notes.html',
+                controller: 'NotesController'
             })
-            .when("/notes/:noteId", {
-                templateUrl: "/partials/note.html",
-                controller: "NotesController"
+            .when('/notes/:noteId', {
+                templateUrl: '/partials/note.html',
+                controller: 'NotesController'
             })
             
             // Beerlist
-            .when("/beer", {templateUrl: "/partials/beer.html"})
+            .when('/beer', {templateUrl: '/partials/beer.html'})
             
             // Wishlist
-            .when("/wishlist", {templateUrl: "/partials/wishlist.html"})
+            .when('/wishlist', {templateUrl: '/partials/wishlist.html'})
             
             // Moria
-            .when("/moria", {templateUrl: "/partials/moria.html"})
-            .when("/moria/:name", {
+            .when('/moria', {templateUrl: '/partials/moria.html'})
+            .when('/moria/:name', {
                 templateUrl: function(urlattr) {
-                    return "/partials/moria/" + urlattr.name;
+                    return '/partials/moria/' + urlattr.name;
                 }
             })
             
             // Fallthrough
-            .otherwise({redirectTo: "/"});
+            .otherwise({redirectTo: '/'});
     }]);
+
+    app.filter('reverse', function() {
+        return function(items) {
+            return items.slice().reverse();
+        };
+    });
 })();
